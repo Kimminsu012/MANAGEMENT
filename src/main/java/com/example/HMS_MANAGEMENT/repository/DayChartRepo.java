2692041c7fadc10df279b82fb26365cc812d7db6
Kimminsu012 <asdfg002269@gmail.com>
2024-05-21 16:28:39 +0900

    css 수정

--- src/main/java/com/example/HMS_MANAGEMENT/repository/DayChartRepo.java
+++ src/main/java/com/example/HMS_MANAGEMENT/repository/DayChartRepo.java
@@ -2,14 +2,17 @@ package com.example.HMS_MANAGEMENT.repository;
 
 
 import com.example.HMS_MANAGEMENT.entity.DayChartEntity;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface DayChartRepo extends JpaRepository<DayChartEntity, Long> {
 
+    Optional<DayChartEntity> findByDate(LocalDate date);
 }
